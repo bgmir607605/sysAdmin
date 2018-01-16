76922cbff1f01dfa7f29d8158e8fc80f9a48e6b9
bgmir607605 <bgmir213@gmail.com>
2018-01-16 15:31:44 +0300

    Скрипт автоматического создания БД

---
+++ addDBs.php
@@ -0,0 +1,24 @@
+<?php
+$rootPass = 'xxxx';
+$prefix = 'tp15_';
+$query = "select * from users where id > 50";
+
+
+$mysqli = new mysqli('localhost', 'root', $rootPass, 'server');
+$mysqli->query("set names utf8");
+$query = "select * from users where id > 50";
+$results = $mysqli->query($query);
+
+while($row = $results->fetch_assoc()){
+        $login = $prefix.''.$row["login"];
+        $pass = $row["pass"];
+        $query = 'create database '.$login.';';
+        $mysqli->query($query);
+        $query = "CREATE USER '$login'@'localhost' IDENTIFIED BY '$pass';";
+        $mysqli->query($query);
+        $query = "GRANT ALL PRIVILEGES ON `$login`.* TO '$login'@'localhost';";
+        $mysqli->query($query);
+        $query = 'FLUSH PRIVILEGES;';
+        $mysqli->query($query);
+}
+?>
